3fb8d05a6c848137e9086a03a404e0f5da985fe0
YunYoung Choi <68066598+yunyoung0531@users.noreply.github.com>
2023-02-28 18:47:11 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
+
+//1. 전체 상품 페이지, 로그인, 상품 상세 페이지
+//2. 전체 상품 페이지에서는 전체 상품을 볼 수 있다.
+//3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
+//4. 상품 디테일을 눌렀으나, 로그인이 안 되어 있을 경우에는 로그엔 페이지가 먼저 나온다.
+//5. 로그인이 되어 있을 경우에는 상품 디테일 페이지를 볼 수 있다.
+//6. 로그아웃 버튼을 클릭하면 로그아웃이 된다.
+//7. 로그아웃이 되면 상품 디테일 페이지를 볼 수 없다. 다시 로그인 페이지가 보인다.
+//8. 로그인을 하면 로그아웃 버튼이 보이고 로그아웃을 하면 로그인 버튼이 보인다.
+//9. 상품을 검색 할 수 있다.
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<ProductAll/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/product/:id" element={<ProductDetail/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
